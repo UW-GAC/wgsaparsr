732952fbc6763d1655bbf20eb2ca4e6f7ead765e
Ben Heavner <bheavner@gmail.com>
2017-08-29 16:04:01 -0700

    This commit is a major refactoring of parse_to_file()
    Refactoring includes moving most functionality to helper functions,
    modularizing code for improved readability and reuse. It enables parsing
    of older WGSA headers or newer ones, and gets rid of hashing for
    uniqueness because db schema has changed.

--- tests/testthat/test-parse-to-file.R
+++ tests/testthat/test-parse-to-file.R
@@ -4,11 +4,13 @@ test_that(
   "parse_to_file returns an error if ", {
     target_columns <- c("fake_field", "pos")
     columns_to_split <- "VEP_ensembl_Transcript_ID"
+    error_msg <- paste0("to_split columns missing in desired_columns: ",
+                        "VEP_ensembl_Transcript_ID", sep = "")
     expect_error(parse_to_file("1k_annotation.gz",
                                destination = "parsed_chr_1.csv",
                                desired_columns = target_columns,
                                to_split = columns_to_split,
                                chunk_size = 1000),
-                 "Not all desired columns are in source file")
+                 error_msg)
   }
 )
