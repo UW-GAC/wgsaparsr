5720f4bc84c16c558c06a30e245f3b87e138496e
Ben Heavner <bheavner@gmail.com>
2017-10-24 11:35:16 -0700

    vectorizing .parse_indel_max_columns()

--- tests/testthat/test-parse-indel-max-columns.R
+++ tests/testthat/test-parse-indel-max-columns.R
@@ -10,9 +10,9 @@ test_that(".parse_indel_max_columns returns expected tibble", {
   target <- dplyr::tibble(
     "a_col" = c("1;2",
                 "42"),
-    "col_to_max_unparsed" = c("1{23};2{1}",
-                     "7"),
     "col_to_max" = c("2",
+                     "7"),
+    "col_to_max_unparsed" = c("1{23};2{1}",
                      "7")
   )
   result <- .parse_indel_max_columns(example, "col_to_max")
