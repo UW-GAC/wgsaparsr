f02f9f59ba68ddab47ff9dea832d991605265e51
Ben Heavner <bheavner@gmail.com>
2018-05-07 12:09:44 -0700

    close #72

--- tests/testthat/test-has-header.R
+++ tests/testthat/test-has-header.R
@@ -2,15 +2,7 @@ context("test_.has_header - unit tests")
 
 test_that(
   ".has_header returns TRUE for a header line", {
-    test_string <- paste("#chr", "pos", "ref", "alt", sep = "\t")
-    expect_true(.has_header(test_string))
-  }
-)
-
-
-test_that(
-  ".has_header returns TRUE for a header line", {
-    test_string <- paste("chr", "pos", "ref", "alt", sep = "\t")
+    test_string <- paste("CHROM", "POS", "REF", "ALT", sep = "\t")
     expect_true(.has_header(test_string))
   }
 )
