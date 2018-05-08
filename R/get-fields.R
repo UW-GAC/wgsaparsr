d12e6041b7b2956a7fdad93e6f05a596f4499fca
Ben Heavner <bheavner@gmail.com>
2018-05-08 10:51:19 -0700

    close #76

--- R/get-fields.R
+++ R/get-fields.R
@@ -1,13 +1,13 @@
 #' Get column names from the WGSA output file
-#' 
-#' \href{https://sites.google.com/site/jpopgen/wgsa}{WGSA} output files can 
-#' contain thousands of fields, including fields with lists of entries. This 
+#'
+#' \href{https://sites.google.com/site/jpopgen/wgsa}{WGSA} output files can
+#' contain thousands of fields, including fields with lists of entries. This
 #' function reads the header of a WGSA file and returns a list of fields in the file.
-#' 
+#'
 #' @param source Path to the WGSA output file to parse
 #' @return a character vector with the names of the WGSA fields
 #'
-#' @examples 
+#' @examples
 #' \dontrun{
 #' all_fields <- get_fields(soure = "WGSA_chr_1.gz")
 #' }
@@ -18,7 +18,9 @@ get_fields <- function(source) {
   header <- suppressWarnings(readLines(readfile_con, n = 1))
   close(readfile_con)
   fields <- stringr::str_split(header, "\t", simplify = TRUE)
-  if (!any(fields[[1]] == "#chr" | fields[[1]] == "chr")) {
+  if (!any(fields[[1]] == "#chr" |
+           fields[[1]] == "chr" |
+           fields[[1]] == "CHROM")) {
     stop("First line of source doesn't look like a WGSA header")
   }
   return(fields)
