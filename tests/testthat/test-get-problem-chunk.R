13953f88846fe7d5af3588ac66913a38bc6ed9d7
Ben Heavner <bheavner@gmail.com>
2018-05-08 14:32:48 -0700

    try to fix test again

--- tests/testthat/test-get-problem-chunk.R
+++ tests/testthat/test-get-problem-chunk.R
@@ -3,7 +3,7 @@ context("test_get_problem_chunk - unit tests")
 test_that(
   "get_problem_chunk returns the expected first chunk", {
     load("loaded_chunk.RDa")
-    expect_identical(loaded_chunk,
+    expect_equivalent(loaded_chunk,
                      suppressMessages(
                        get_problem_chunk("1k_annotation.gz", 5, 1)))
   }
@@ -12,7 +12,7 @@ test_that(
 test_that(
   "get_problem_chunk returns the expected second chunk", {
     load("loaded_chunk_2.RDa")
-    expect_identical(loaded_chunk_2,
+    expect_equivalent(loaded_chunk_2,
                      suppressMessages(
                        get_problem_chunk("1k_annotation.gz", 5, 2)))
   }
